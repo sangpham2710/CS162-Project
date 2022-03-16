f0101dfc437f9c1e26ea30f27fda2b5a3412b46f
NhutThi2404 <nhutth2404@gmail.com>
2022-03-17 02:03:23 +0700

    add class.h

--- data/study-slot.h
+++ data/study-slot.h
@@ -1,15 +1,17 @@
 #pragma once
 #include "student.h"
+#include "class.h"
 #include<iostream>
 using namespace std;
 
-class StudySlot: public Student {
+class StudySlot: public Student, public Class{
  private:
-  string classID;
   StudySlot(int _studentID, string _studentName, string _gender,
             string _dayOfBirth, string _studentSpecialty, string _username,
-            string _password, string _typeOfAccount, string _classID)
+            string _password, string _typeOfAccount, int _classID,
+            int _totalStudent, int _totalMaleStudent, int _totalFemaleStudent)
       : Student(_studentID, _studentName, _gender, _dayOfBirth,
                 _studentSpecialty, _username, _password, _typeOfAccount),
-        classID(_classID) {}
+        Class(_classID, _totalStudent, _totalMaleStudent,
+              _totalFemaleStudent) {}
 };
