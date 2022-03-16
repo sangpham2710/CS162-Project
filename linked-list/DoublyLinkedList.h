b18a5bf477f8fdbbefa478752fe679dbaef99a99
REK7on <tansang.27.10.2003@gmail.com>
2022-03-16 20:14:40 +0700

    add base class for linked list

--- linked-list/DoublyLinkedList.h
+++ linked-list/DoublyLinkedList.h
@@ -8,8 +8,140 @@
 #include <string>
 
 #include "Node.h"
+template <class T>
+class List {
+ public:
+  class iterator;
+  class const_iterator;
 
+ private:
+  iterator list_begin, list_end;
+  int list_size;
+  bool insert_next(iterator& it, iterator& it_next) {
+    if (it == this->end() || it == nullptr || it_next == this->end() ||
+        it_next == nullptr)
+      return false;
 
+    Node<T>*& node = it.ptr;
+    Node<T>*& p_next = it_next.ptr;
+
+    p_next->next = node->next;
+    p_next->prev = node;
+    p_next->prev->next = p_next;
+    p_next->next->prev = p_next;
+
+    ++this->list_size;
+
+    return true;
+  }
+  bool insert_previous(const iterator& it, const iterator& it_prev) {
+    if (it.ptr == nullptr || it_prev == this->end() || it_prev.ptr == nullptr)
+      return false;
+
+    Node<T>* node = it.ptr;
+    Node<T>* p_prev = it_prev.ptr;
+
+    p_prev->next = node;
+    p_prev->prev = node->prev;
+    p_prev->next->prev = p_prev;
+    if (p_prev->prev) p_prev->prev->next = p_prev;
+
+    if (it == this->begin()) list_begin = p_prev;
+
+    ++this->list_size;
+
+    return true;
+  }
+  bool remove(iterator& it) {
+    if (it == this->end() || it.ptr == nullptr) return false;
+
+    Node<T>* node = it.ptr;
+
+    if (node->next) {
+      node->next->prev = node->prev;
+    }
+    if (node->prev) {
+      node->prev->next = node->next;
+    }
+
+    if (this->begin() == it) {
+      ++list_begin;
+    }
+
+    --this->list_size;
+
+    delete node;
+    it.ptr = nullptr;
+
+    return true;
+  }
+  /// Exception(s): out of range
+  iterator get_iterator(const int& index) {
+    if (index < 0 || index >= this->size()) {
+      throw std::out_of_range("List of size " + std::to_string(this->size()) +
+                              " is out of range at index " +
+                              std::to_string(index) + ".");
+    }
+
+    iterator it;
+    if (index < this->size() / 2) {
+      it = this->begin();
+      for (int i = 1; i <= index; i++) ++it;
+    } else {
+      it = this->end();
+      for (int i = this->size() - 1; i >= index; i--) --it;
+    }
+
+    return it;
+  }
+  const_iterator get_iterator(const int& index) const {
+    if (index < 0 || index >= this->size()) {
+      throw std::out_of_range("List of size " + std::to_string(this->size()) +
+                              " is out of range at index " +
+                              std::to_string(index) + ".");
+    }
+
+    const_iterator it;
+    if (index < this->size() / 2) {
+      it = this->begin();
+      for (int i = 1; i <= index; i++) ++it;
+    } else {
+      it = this->end();
+      for (int i = this->size() - 1; i >= index; i--) --it;
+    }
+
+    return it;
+  }
+
+ public:
+  List() : list_size{0} {
+    Node<T>* end_node = new Node<T>();
+    list_begin = list_end = end_node;
+  }
+  List(const std::initializer_list<T>& source) : list_size{0} {
+    Node<T>* end_node = new Node<T>();
+    list_begin = list_end = end_node;
+    for (const auto& x : source) this->push_back(x);
+  }
+  List(const List<T>& source) : list_size{0} {
+    Node<T>* end_node = new Node<T>();
+    list_begin = list_end = end_node;
+
+    for (const auto& x : source) this->push_back(x);
+  }
+  List(List<T>&& source)
+      : list_begin{source.list_begin},
+        list_end{source.list_end},
+        list_size{source.list_size} {
+    source.list_begin = nullptr;
+    source.list_end = nullptr;
+    source.list_size = 0;
+  }
+  ~List() {
+    this->clear();
+    delete list_end.ptr;
+  }
+};
 template <class T>
 class List<T>::iterator
     : public std::iterator<std::bidirectional_iterator_tag, T, T, T*, T&> {
