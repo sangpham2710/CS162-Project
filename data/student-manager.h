4e93381a1f23cde6cb2d6b571d64875d98b31420
NhutThi2404 <nhutth2404@gmail.com>
2022-03-16 10:53:48 +0700

    fix error in student-manager.h

--- data/student-manager.h
+++ data/student-manager.h
@@ -4,16 +4,17 @@
 #include "account.h"
 using namespace std;
 
-class StudentManager {
+class StudentManager : public Account {
  private:
   int managerID;
   string managerName;
   string gender;
   string dayOfBirth;  // Need to fix
   StudentManager(int _managerID, string _managerName, string _gender,
-                 string _dayOfBirth, string _username)
+                 string _dayOfBirth, string _username, string _password,
+                 string _typeOfAccount)
       : Account(_username, _password, _typeOfAccount),
-        managerID(_managerID),
+       managerID(_managerID),
         managerName(_managerName),
         gender(_gender),
         dayOfBirth(_dayOfBirth) {}
