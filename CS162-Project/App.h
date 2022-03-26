c0a286bd13405f6977de6eb4df71580c5df7ac65
REK7on <tansang.27.10.2003@gmail.com>
2022-03-26 21:42:29 +0700

    merge Global.cpp and App.cpp

--- CS162-Project/App.h
+++ CS162-Project/App.h
@@ -1,9 +1,29 @@
 #pragma once
 
+#include "Class.h"
+#include "Course.h"
+#include "List.h"
+#include "SchoolYear.h"
+#include "Semester.h"
+#include "Student.h"
+#include "User.h"
+
 class App {
  public:
-  static void loadData();
-  static void saveData();
+  static User* pCurrentUser;
+  static Semester* pCurrentSemester;
+  static Semester* pRecentSemester;
+  static List<User*> pUsers;
+  static List<SchoolYear*> pSchoolYears;
+  static List<Semester*> pSemesters;
+  static List<Course*> pCourses;
+  static List<Class*> pClasses;
+  static List<Student*> pStudents;
+
+  static void allocate();
+  static bool loadData();
   static void main();
+  static bool saveData();
+  static void deallocate();
   static void run();
 };
