32ab767fc5343b87c3ed7d35523db64d34513f48
REK7on <tansang.27.10.2003@gmail.com>
2022-03-26 21:48:31 +0700

    implement << for course, student, user, courseMark

--- CS162-Project/Course.h
+++ CS162-Project/Course.h
@@ -5,26 +5,28 @@
 
 #include "ID.h"
 #include "List.h"
-#include "Student.h"
 
 using std::string;
+
+class Semester;
+class Student;
 
 class Course {
  public:
   string _id;
-  string semesterID;
   string courseCode;
   string courseName;
   string lecturer;
   string startDate;
   string endDate;
-  int maxNumberOfStudents = 0;
-  int numberOfCredits = 0;
+  int maxNumberOfStudents;
+  int numberOfCredits;
   string schedule;
-
-  List<List<Student>::iterator> students;
+  Semester* pSemester;
+  List<Student*> pStudents;
 
   Course() : _id(ID::gen()) {}
+  friend std::ostream& operator<<(std::ostream& stream, const Course& course);
   void viewScoreboard();
   void importScoreboard();
   void exportScoreboard();
--- CS162-Project/CourseMark.h
+++ CS162-Project/CourseMark.h
@@ -2,12 +2,22 @@
 
 #include <string>
 
+#include "Course.h"
+
 using std::string;
 
 class Course;
 
 class CourseMark {
  public:
-  List<Course>::iterator pCourse;
+  Course* pCourse;
   double midtermMark, finalMark, otherMark, totalMark;
+  CourseMark()
+      : pCourse(nullptr),
+        midtermMark(0),
+        finalMark(0),
+        otherMark(0),
+        totalMark(0) {}
+  friend std::ostream& operator<<(std::ostream& stream,
+                                  const CourseMark& courseMark);
 };
