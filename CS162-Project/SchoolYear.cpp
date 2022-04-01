a6fccbc4924f8367b5ec9f3ff4a2262e43169530
ggnohope <lamhoa9a1thcsttlp@gmail.com>
2022-04-01 22:05:09 +0700

    add create schoolyear/semester menu, main schoolyear menu, choose schoolyear

--- CS162-Project/SchoolYear.cpp
+++ CS162-Project/SchoolYear.cpp
@@ -1,35 +1,141 @@
 #include "SchoolYear.h"
 
 #include "App.h"
+#include "Console.h"
+#include "Menu.h"
 
 std::istream& operator>>(std::istream& stream, SchoolYear& schoolYear) {
-  int n;
-  stream >> schoolYear._id;
-  stream.ignore();
-  getline(stream, schoolYear.yearName);
-  stream >> n;
-  for (int i = 0; i < n; ++i) {
-    string semesterID;
-    stream >> semesterID;
-    schoolYear.pSemesters.push_back(*App::pSemesters.find_if(
-        [&](const auto& p) { return p->_id == semesterID; }));
-  }
-  stream >> n;
-  for (int i = 0; i < n; ++i) {
-    string classID;
-    stream >> classID;
-    schoolYear.pClasses.push_back(*App::pClasses.find_if(
-        [&](const auto& p) -> bool { return p->_id == classID; }));
-  }
-  return stream;
+    int n;
+    stream >> schoolYear._id;
+    stream.ignore();
+    getline(stream, schoolYear.yearName);
+    stream >> n;
+    for (int i = 0; i < n; ++i) {
+        string semesterID;
+        stream >> semesterID;
+        schoolYear.pSemesters.push_back(*App::pSemesters.find_if(
+            [&](const auto& p) { return p->_id == semesterID; }));
+    }
+    stream >> n;
+    for (int i = 0; i < n; ++i) {
+        string classID;
+        stream >> classID;
+        schoolYear.pClasses.push_back(*App::pClasses.find_if(
+            [&](const auto& p) -> bool { return p->_id == classID; }));
+    }
+    return stream;
 }
 
 std::ostream& operator<<(std::ostream& stream, const SchoolYear& schoolYear) {
-  stream << schoolYear._id << '\n';
-  stream << schoolYear.yearName << '\n';
-  stream << schoolYear.pSemesters.size() << '\n';
-  for (const auto& p : schoolYear.pSemesters) stream << p->_id << '\n';
-  stream << schoolYear.pClasses.size() << '\n';
-  for (const auto& p : schoolYear.pClasses) stream << p->_id << '\n';
-  return stream;
+    stream << schoolYear._id << '\n';
+    stream << schoolYear.yearName << '\n';
+    stream << schoolYear.pSemesters.size() << '\n';
+    for (const auto& p : schoolYear.pSemesters) stream << p->_id << '\n';
+    stream << schoolYear.pClasses.size() << '\n';
+    for (const auto& p : schoolYear.pClasses) stream << p->_id << '\n';
+    return stream;
+}
+
+void SchoolYear::choose(SchoolYear* pSchoolYear, short screen, short option) {
+    if (screen == 1) { // schoolYearMainMenu
+        if (option >= 1 && option <= App::pSchoolYears.size()) {
+            App::pSchoolYears[option - 1]->schoolYearChooseMenu();
+            return;
+        }
+        if (option == App::pSchoolYears.size() + 1) {
+        //    create();
+            return;
+        }
+        else {
+            Menu::staffMenu();
+            return;
+        }
+    }
+    if (screen == 2) { // schoolYearChooseMenu
+        switch (option) {
+        case 0: {
+            schoolYearMainMenu();
+            break;
+        }
+        case 1: {
+
+            break;
+        }
+        }
+    }
+}
+
+void SchoolYear::SchoolYearSemesterMenu() {
+    Console::clear();
+
+    cout << "---------------------------------------\n";
+    cout << "1. School Years\n";
+    cout << "2. Semesters\n";
+    cout << "---------------------------------------\n";
+    cout << "0. Return\n";
+    cout << "Your choice: ";
+    short option;
+    cin >> option;
+    while (option < 0 || option > 2) {
+        cout << "\nInvalid!\n";
+        cout << "Your choice: ";
+        cin >> option;
+    }
+    if (option == 0) {
+        Menu::staffMenu;
+    }
+    if (option == 1) {
+        schoolYearMainMenu();
+        return;
+    }
+    if (option == 2) {
+
+    }
+}
+
+void SchoolYear::schoolYearMainMenu() {
+    Console::clear();
+
+    int i = 1;
+    cout << "---------------------------------------\n";
+    for (const auto& p : App::pSchoolYears) {
+        cout << i << ". " << p->yearName << "\n";
+        ++i;
+    }
+    cout << "---------------------------------------\n";
+    cout << i << ". "
+        << "Add new school year \n";
+    cout << 0 << ". " << "Return \n \n";
+    cout << "Your choice: ";
+
+    short option;
+    cin >> option;
+    while (option < 0 || option > i) {
+        cout << "\nInvalid!\n";
+        cout << "Your choice: ";
+        cin >> option;
+    }
+    choose(NULL, 1, option);
+}
+
+void SchoolYear::schoolYearChooseMenu() {
+    Console::clear();
+
+    cout << "---------------------------------------\n";
+    cout << "1. Update school year\n";
+    cout << "2. Delete school year\n";
+    cout << "---------------------------------------\n";
+    cout << "0. Return\n";
+
+    cout << "Your choice: ";
+    short option;
+    cin >> option;
+    while (option < 0 || option > 2) {
+        cout << "\nInvalid!\n";
+        cout << "Your choice: ";
+        cin >> option;
+    }
+
+    choose(this, 2, option);
+    return;
 }
