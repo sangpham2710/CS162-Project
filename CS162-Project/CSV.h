2ae9b329c4a73b03d055e157f7281d9aa4803a70
REK7on <tansang.27.10.2003@gmail.com>
2022-04-12 20:09:49 +0700

    add readline function

--- CS162-Project/CSV.h
+++ CS162-Project/CSV.h
@@ -19,3 +19,82 @@ enum Error {
   UnclosedQuote,
   InsufficientColumns,
 };
+
+Error _readLine(const string &line, List<string> &values) {
+  string value = "";
+  auto state = State::Initial;
+
+  for (const char &c : line) {
+    switch (state) {
+      case State::Initial: {
+        switch (c) {
+          case ',':
+            values.push_back("");
+            break;
+          case '"':
+            state = State::Quote;
+            break;
+          default:
+            value += c;
+            state = State::Data;
+            break;
+        }
+        break;
+      }
+      case State::Data: {
+        switch (c) {
+          case ',':
+            values.push_back(value);
+            value = "";
+            state = State::Initial;
+            break;
+          case '"':
+            return Error::ImproperQuote;
+          default:
+            value += c;
+            break;
+        }
+        break;
+      }
+      case State::Quote: {
+        switch (c) {
+          case '"':
+            state = State::QuoteInQuote;
+            break;
+          default:
+            value += c;
+            break;
+        }
+        break;
+      }
+      case State::QuoteInQuote: {
+        switch (c) {
+          case ',':
+            values.push_back(value);
+            value = "";
+            state = State::Initial;
+            break;
+          case '"':
+            value += c;
+            state = State::Quote;
+            break;
+          default:
+            return Error::UnpairedQuote;
+        }
+        break;
+      }
+    }
+  }
+
+  switch (state) {
+    case State::Initial:
+    case State::Data:
+    case State::QuoteInQuote:
+      values.push_back(value);
+      break;
+    case State::Quote:
+      return Error::UnpairedQuote;
+  }
+
+  return Error::Success;
+}
