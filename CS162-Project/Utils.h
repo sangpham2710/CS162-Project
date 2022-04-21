734c71bc5b9f3ca5d31a6e8a3c6047720f31ac93
REK7on <tansang.27.10.2003@gmail.com>
2022-04-21 13:30:17 +0700

    Revert "fix input bug"
    This reverts commit ce1257446225b2f7999525c9507e38e9500d4db2.

--- CS162-Project/Utils.h
+++ CS162-Project/Utils.h
@@ -8,9 +8,15 @@
 class Utils {
  public:
   static void waitForKeypress() {
-    cout << "Press any key to continue\n";
-    char ch = _getch();
-    if (ch == 0 || ch == 224) ch = _getch();
+#pragma push_macro("max")
+#undef max
+    if (cin.rdbuf()->sungetc() != std::char_traits<char>::eof() &&
+        cin.get() != '\n') {
+      cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    cout << "Press Enter to continue\n";
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+#pragma pop_macro("max")
   }
   static int getOption(int minOption, int maxOption) {
     int option;
